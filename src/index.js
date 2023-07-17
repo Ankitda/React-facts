a652dedf7ddb8b3c8a40ce940a88c879c343e0a0
Ankit Das <ankitsonu240@gmail.com>
2023-07-17 22:06:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const name = "Learner";
+ 
+const element = <h1>Hello,
+{ name }.Welcome to GeeksforGeeks.</h1>;
+  
+ReactDOM.render(
+    element,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
