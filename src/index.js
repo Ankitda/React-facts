f1cdc66c791b21ba8a71dee9da00e0d96089cd4c
Ankit Das <ankitsonu240@gmail.com>
2023-07-17 22:13:35 +0530

    changing code

--- src/index.js
+++ src/index.js
@@ -16,11 +16,6 @@ root.render(
 //   </React.StrictMode>
 );
 
- 
-  
-// ReactDOM.render(
-//     document.getElementById("root")
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
